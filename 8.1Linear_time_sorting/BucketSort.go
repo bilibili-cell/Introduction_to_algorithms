06281fdd40e31b5069d29bd667bc197d55735fb7
Junyue_Wang <stream886199@163.com>
2025-02-16 21:18:10 +0800

    Add:Implement bucket sorting

--- 8.1Linear_time_sorting/BucketSort.go
+++ 8.1Linear_time_sorting/BucketSort.go
@@ -4,3 +4,52 @@ type Node struct {
 	value float64
 	Next  *Node
 }
+
+func BucketSort(arr *[]int) {
+	listArray := *arr
+	assistArray := make([]Node, 10)
+	for i := 0; i < 10; i++ {
+		assistArray[i] = Node{
+			value: 0,
+			Next:  nil,
+		}
+	}
+	for _, item := range listArray {
+		node := Node{
+			value: item,
+			Next:  nil,
+		}
+		value := assistArray[item/10].value
+		listNode := assistArray[item/10].Next
+		pre := &assistArray[item/10]
+		for i := 0; i < value; i++ {
+			if listNode != nil {
+				v := listNode.value
+				if node.value < v {
+					node.Next = pre.Next
+					pre.Next = &node
+				} else {
+					pre = listNode
+					listNode = listNode.Next
+				}
+			} else {
+				break
+			}
+		}
+		assistArray[item/10].value++
+
+	}
+	i := 0
+	for _, item := range assistArray {
+
+		if item.Next != nil {
+			for j := 1; j <= item.value; j++ {
+				listArray[i] = item.Next.value
+				item = *item.Next
+				i++
+			}
+		}
+
+	}
+
+}
