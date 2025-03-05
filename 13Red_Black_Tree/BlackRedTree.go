36621b6a284b7c53660059c7438eb610f5dfcf91
Junyue_Wang <stream886199@163.com>
2025-03-05 23:11:38 +0800

    Add:add RBTree Insert function Code

--- 13Red_Black_Tree/BlackRedTree.go
+++ 13Red_Black_Tree/BlackRedTree.go
@@ -1,10 +1,16 @@
 package Red_Black_Tree
 
+const (
+	RED   = true
+	BLACK = false
+)
+
 type Node struct {
 	Value  int
+	Color  bool
 	Left   *Node
 	Right  *Node
-	Parent *Node // 添加 Parent 指针用于维护父节点
+	Parent *Node
 }
 
 type RedBlackTree struct {
@@ -32,11 +38,13 @@ func (tree *RedBlackTree) LeftRotate(x *Node) {
 	y.Left = x
 	x.Parent = y
 }
+
 func (tree *RedBlackTree) RightRotate(x *Node) {
 	y := x.Left
 	if y == nil {
 		return
 	}
+
 	x.Left = y.Right
 	if y.Right != nil {
 		y.Right.Parent = x
@@ -46,10 +54,78 @@ func (tree *RedBlackTree) RightRotate(x *Node) {
 		tree.Head = y
 	} else if x == x.Parent.Left {
 		x.Parent.Left = y
-	} else if x == x.Parent.Right {
+	} else {
 		x.Parent.Right = y
 	}
-	x.Right = y
-	y.Parent = x
+	y.Right = x
+	x.Parent = y
+}
 
+func (tree *RedBlackTree) Insert(value int) {
+	newNode := &Node{Value: value, Color: RED}
+	if tree.Head == nil {
+		tree.Head = newNode
+		tree.Head.Color = BLACK
+		return
+	}
+
+	insertNode(tree.Head, newNode)
+	tree.fixInsert(newNode)
+}
+
+func insertNode(root, node *Node) {
+	if node.Value < root.Value {
+		if root.Left == nil {
+			root.Left = node
+			node.Parent = root
+		} else {
+			insertNode(root.Left, node)
+		}
+	} else {
+		if root.Right == nil {
+			root.Right = node
+			node.Parent = root
+		} else {
+			insertNode(root.Right, node)
+		}
+	}
+}
+
+func (tree *RedBlackTree) fixInsert(node *Node) {
+	for node != tree.Head && node.Parent.Color == RED {
+		if node.Parent == node.Parent.Parent.Left {
+			uncle := node.Parent.Parent.Right
+			if uncle != nil && uncle.Color == RED {
+				node.Parent.Color = BLACK
+				uncle.Color = BLACK
+				node.Parent.Parent.Color = RED
+				node = node.Parent.Parent
+			} else {
+				if node == node.Parent.Right {
+					node = node.Parent
+					tree.LeftRotate(node)
+				}
+				node.Parent.Color = BLACK
+				node.Parent.Parent.Color = RED
+				tree.RightRotate(node.Parent.Parent)
+			}
+		} else {
+			uncle := node.Parent.Parent.Left
+			if uncle != nil && uncle.Color == RED {
+				node.Parent.Color = BLACK
+				uncle.Color = BLACK
+				node.Parent.Parent.Color = RED
+				node = node.Parent.Parent
+			} else {
+				if node == node.Parent.Left {
+					node = node.Parent
+					tree.RightRotate(node)
+				}
+				node.Parent.Color = BLACK
+				node.Parent.Parent.Color = RED
+				tree.LeftRotate(node.Parent.Parent)
+			}
+		}
+	}
+	tree.Head.Color = BLACK
 }
